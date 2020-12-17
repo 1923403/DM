d5dfb5c14f5dca675a8127cc04b14befbb1044ef
steff <steff@Steffen-L480.fritz.box>
2020-12-17 14:26:35 +0100

    up

--- DM/src/jpa/Professor.java
+++ DM/src/jpa/Professor.java
@@ -3,12 +3,12 @@ package jpa;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Professor")
@@ -33,6 +33,7 @@ public class Professor extends Person {
 		this.vorlesungen.add(v);
 	}
 
+	@Override
 	public String getName() {
 		return this.name;
 	}
@@ -45,6 +46,7 @@ public class Professor extends Person {
 		return this.vorlesungen;
 	}
 
+	@Override
 	public void setName(String name) {
 		this.name = name;
 	}
--- DM/src/jpa/TestJPALesen.java
+++ DM/src/jpa/TestJPALesen.java
@@ -1,9 +1,9 @@
 package jpa;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.Query;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
 
 public class TestJPALesen {
 
